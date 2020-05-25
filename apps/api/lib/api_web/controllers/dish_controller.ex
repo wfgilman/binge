753fbdedc1047965118fc034f589e9cdeaddcf60
Will Gilman <will@samadhi.tech>
2020-05-24 17:28:20 -0700

    Shuffle response randomly.

--- apps/api/lib/api_web/controllers/dish_controller.ex
+++ apps/api/lib/api_web/controllers/dish_controller.ex
@@ -1,10 +1,16 @@
 defmodule ApiWeb.DishController do
   use ApiWeb, :controller
 
-  def index(conn, params) do
+  def index(conn, _params) do
+    data =
+      Db.Model.Dish
+      |> Db.Repo.all()
+      |> Db.Repo.preload(:restaurant)
+      |> Enum.shuffle()
+
     conn
     |> put_status(200)
     |> put_view(ApiWeb.DishView)
-    |> render("index.json", data: Db.Repo.all(Db.Model.Dish) |> Db.Repo.preload(:restaurant))
+    |> render("index.json", data: data)
   end
 end
