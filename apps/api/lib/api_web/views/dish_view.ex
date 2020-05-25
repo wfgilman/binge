2cec631209fbaaeee668b8b662e3bcab28601dae
Will Gilman <will@samadhi.tech>
2020-05-24 17:28:50 -0700

    Drop the top level data structure for easier client parsing.

--- apps/api/lib/api_web/views/dish_view.ex
+++ apps/api/lib/api_web/views/dish_view.ex
@@ -2,20 +2,15 @@ defmodule ApiWeb.DishView do
   use ApiWeb, :view
 
   def render("index.json", %{data: dishes}) do
-    %{
-      object: "dish",
-      data: Enum.map(dishes, &dish_json/1)
-    }
-  end
-
-  defp dish_json(dish) do
-    %{
-      id: dish.id,
-      name: dish.name,
-      type: dish.type,
-      image_url: dish.image_url,
-      restaurant_name: dish.restaurant.name,
-      restaurant_category: dish.restaurant.category
-    }
+      Enum.map(dishes, fn dish ->
+        %{
+          id: dish.id,
+          name: dish.name,
+          type: dish.type,
+          image_url: dish.image_url,
+          restaurant_name: dish.restaurant.name,
+          doordash_url: dish.restaurant.doordash_url
+        }
+      end)
   end
 end
