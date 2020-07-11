1c4859be893f7e83090e98c4cea61fda63479457
Will Gilman <will@samadhi.tech>
2020-07-10 18:09:13 -0700

    Updated twilio access token now that repo is private

--- apps/notify/config/dev.exs
+++ apps/notify/config/dev.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :ex_twilio,
   account_sid: "ACf5d58cd330f96450b0a9d9e357d0687a",
-  auth_token: "c0bfc14d6527fc0a86de9f37f5fa4d02",
+  auth_token: "1a4a50de26a509f91231eefbb38787c2",
   send_number: "16195667131"
 
 config :pigeon, :apns,
